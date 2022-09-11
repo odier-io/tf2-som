2bb8ffaf0eeaff8ef1687eb95b43af9bab6b733d
Jérôme Odier <jerome.odier@lpsc.in2p3.fr>
2022-09-12 01:01:49 +0200

    update

--- docs.sh
+++ docs.sh
@@ -9,3 +9,12 @@ sed '/<header>/,/<\/header>/d' ./docs/tf_som/index.html > ./docs/index.html
 rm -fr ./docs/tf_som/
 
 ########################################################################################################################
+
+if ! command -v html-minifier-terser &> /dev/null
+then
+  sudo npm install -g html-minifier-terser
+bi
+
+html-minifier-terser --collapse-whitespace --remove-comments --minify-css true --minify-js true -o ./docs/index.html
+
+########################################################################################################################
